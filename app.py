1bf6dab6f1d975655a75f9c12dbfe5634c3f2c95
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-10-07 12:07:05 -0400

    Feat: show all books function

--- app.py
+++ app.py
@@ -2,6 +2,30 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+livros = [
+    {
+        'id': 1,
+        'titulo': 'O Senhor dos Anéis -  A Sociedade do Anel',
+        'autor': 'J. R. R, Tolkien',
+        'quantidade': 5,
+        'disponibilidade': 5
+    },
+    {
+        'id': 2,
+        'titulo': 'A Guerra dos Tronos',
+        'autor': 'George R. R. Martin',
+        'quantidade': 5,
+        'disponibilidade': 5
+    },
+    {
+        'id': 3,
+        'titulo': 'Harry Potter e a câmara secreta',
+        'autor': 'J. K. Rowling',
+        'quantidade': 5,
+        'disponibilidade': 5
+    }
+]
+
 usuarios = [
     {
         'id': 1,
@@ -12,3 +36,18 @@ usuarios = [
         'nome': 'Carlos'
     }
 ]
+
+# Função de consultar livros
+@app.route('/livros')
+def ler_livros():
+    return jsonify(livros)
+
+# Função de consultar livros específicos
+
+# Função de editar livros
+
+# Função de Excluir livros
+
+# Função de Alocar livros
+
+app.run(port=5000, host='localhost', debug=True)
